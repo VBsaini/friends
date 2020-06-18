5096e2551db46b1af24a9cca0a6576b14ce80b2d
vaibhav-saini-Edifice <vbsaini2004gmail.com>
2020-06-18 15:04:39 +0530

    Updates

---
+++ precache-manifest.a4dceaf58aede58206e334f274751269.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf05625f14a9048665f1836f7e5f41f5",
+    "url": "/friends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5ab389f7e96676f21630",
+    "url": "/friends/static/css/main.c0f12637.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/friends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ab389f7e96676f21630",
+    "url": "/friends/static/js/main.c1ecaa1d.chunk.js"
+  },
+  {
+    "revision": "f103641757bff9e46cfc",
+    "url": "/friends/static/js/runtime-main.b8c2f633.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
